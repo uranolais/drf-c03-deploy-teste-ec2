94861b3efd69eb9bbcad3035d0a844f684abcd25
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-02-27 17:09:01 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=hyxm8a5^_z+#0bznj-)_s@ba&zgvrjlyp(z3781vop8)^to46
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
